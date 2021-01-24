fd57a9d21d54e016564dfddecd7fe4fba88dfb13
Komal Mann <mannkomal35@gmail.com>
2021-01-24 22:45:52 +0530

    fix erros

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // 1 create the constants
 
-const quotes = ["Good taste is for people who can't afford sapphires.",
+const quotes = ["Good taste is for people who cannot afford sapphires.",
 
-"May you be in heaven a full half-hour before the devil knows you're dead.",
+"May you be in heaven a full half-hour before the devil knows you are dead.",
 
 "Intelligence is a very valuable thing, my friend. And usually, it comes far too late.",
 
-"Everyone's a whore, Grace. We just sell different parts of ourselves",
+"Everyone is a whore, Grace. We just sell different parts of ourselves",
 
 "You know, gentlemen, there is hell, and there is another place below hell.",
 
 "This place is under new management, by order of the Peaky Blinders.",
 
 
-"I imagine being shot by a woman hurts the same as being shot by a man. It’s just more shameful.",
+"I imagine being shot by a woman hurts the same as being shot by a man. Its just more shameful.",
 
 "You once said to me that men like us can never be loved. She loves me and all you got was a bullet.",
 
-"We're a close family, always within punching distance."];
+"We are a close family, always within punching distance."];
 
 //store the list of words and the index of the word the player is typing.
 
